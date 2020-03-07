2d354a706a999f93156b215b39d835a6d7f07d5e
nanoyatsu <34255833+nredjap@users.noreply.github.com>
2020-03-07 21:19:57 +0900

    delete MainFragment

--- app/src/main/java/com/nanoyatsu/proto/randomWorkManager/MainActivity.kt
+++ app/src/main/java/com/nanoyatsu/proto/randomWorkManager/MainActivity.kt
@@ -2,17 +2,15 @@ package com.nanoyatsu.proto.randomWorkManager
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.nanoyatsu.proto.randomWorkManager.ui.main.MainFragment
+import androidx.databinding.DataBindingUtil
+import com.nanoyatsu.proto.randomWorkManager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
