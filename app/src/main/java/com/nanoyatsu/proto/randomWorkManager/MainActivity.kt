e70802d6932e7be26406ef19f691b66203c78a25
nanoyatsu <34255833+nredjap@users.noreply.github.com>
2020-03-07 20:41:56 +0900

    rename layout files

--- app/src/main/java/com/nanoyatsu/proto/randomWorkManager/MainActivity.kt
+++ app/src/main/java/com/nanoyatsu/proto/randomWorkManager/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.nanoyatsu.proto.randomWorkManager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.nanoyatsu.proto.randomWorkManager.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/com/nanoyatsu/proto/randomWorkManager/ui/main/MainFragment.kt
+++ app/src/main/java/com/nanoyatsu/proto/randomWorkManager/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.nanoyatsu.proto.randomWorkManager.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import com.nanoyatsu.proto.randomWorkManager.R
 
 class MainFragment : Fragment() {
@@ -20,7 +20,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
